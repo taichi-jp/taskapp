36259f9615bbfac08dcfac6b93fee30f3a37fd6e
taichikoumoto <sbskomoto@gmail.com>
2017-06-08 15:43:25 +0900

    initial

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -1,18 +1,12 @@
-//
-//  ViewController.swift
-//  taskapp
-//
-//  Created by 河本泰知 on 2017/06/08.
-//  Copyright © 2017年 taichi.komoto. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
